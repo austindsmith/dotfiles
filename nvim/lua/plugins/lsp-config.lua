278005cc4f7e4ee51a9afed7f1b4b33dbbc56649
Austin Smith <austindsmith_accounts@proton.me>
2025-11-11 18:02:14 -0700

    more plugins, linters, formatters

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,4 +1,28 @@
 return {
-    "mason-org/mason.nvim",
-    opts = {}
+	{
+		"mason-org/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				auto_install = true,
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			vim.lsp.config("lua_ls", {}) -- define/override settings
+			vim.lsp.enable("lua_ls") -- start it for its filetypes
+			vim.lsp.config("tsserver", {}) -- define/override settings
+			vim.lsp.enable("tsserver") -- start it for its filetypes
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
