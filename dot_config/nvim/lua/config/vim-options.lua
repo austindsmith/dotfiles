fd9ff82436da8a16046c5a4007e026b71ca6d17f
Austin Smith <austindsmith_accounts@proton.me>
2026-05-17 20:42:00 -0600

    updated nvim, tmux, zsh and hyprland

--- dot_config/nvim/lua/config/vim-options.lua
+++ dot_config/nvim/lua/config/vim-options.lua
@@ -5,6 +5,7 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
 -- line numbers
-vim.opt.number = true            -- absolute line numbers
-vim.opt.relativenumber = true    -- relative line numbers
-vim.opt.signcolumn = "yes"       -- avoids text shifting
+vim.opt.number = true -- absolute line numbers
+vim.opt.relativenumber = true -- relative line numbers
+vim.opt.signcolumn = "yes" -- avoids text shifting
+vim.opt.clipboard = "unnamedplus" -- use system clipboard
